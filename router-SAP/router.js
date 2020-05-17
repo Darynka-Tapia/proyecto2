4f1beff5d17c9dd9c0f06948e59fd96f318ce194
Darynka Tapia <darytapia09@gmail.com>
2020-05-17 00:21:48 -0500

    Subiendo archivos del curso de router para Single Page Aplication

---
+++ router-SAP/router.js
@@ -0,0 +1,42 @@
+class Router{
+    constructor(routes){
+        this.routes = routes;
+        this._loadInitialRoute();
+    }
+
+    loadRoute(...urlSegs){
+        const matchedRoute = this._matchUrlToRoute(urlSegs);
+        const url = `/${urlSegs.join('/')}`;
+        history.pushState({}, 'this works', url);
+        const routerOutElm = document.querySelectorAll('[data-router]')[0];
+        routerOutElm.innerHTML = matchedRoute.template;
+    }
+
+    _matchUrlToRoute(urlSegs){
+        const matchedRoute = this.routes.find(route => {
+            const routePathSegs = route.path.split('/').slice(1);
+            if(routePathSegs.length !== urlSegs.length){
+                return false;
+            }
+            return routePathSegs
+                .every((routePathSegs, i) => routePathSegs === urlSegs[i]);
+        });
+
+        if (matchedRoute === undefined) {
+            return {
+                template: '<h1>404 Pagina no encontrada</h1>'
+            };
+        }
+
+        return matchedRoute;
+    }
+
+    _loadInitialRoute(){    
+        const pathNameSplit = window.location.pathname.split('/');
+        const pathSegs = pathNameSplit.length > 1 ? pathNameSplit.slice(1) : '';
+        
+        this.loadRoute(...pathSegs);
+    }
+}
+
+
---
+++ router-SAP/routes.js
@@ -0,0 +1,15 @@
+const routes = [
+    {
+        path: '/',
+        template: '<img src="img/persona.png">'
+    },
+    {
+        path: '/dos',
+        template: '<img src="img/persona.png"> <img src="img/persona.png">'        
+    },
+    {
+        path: '/tres',
+        template: '<img src="img/persona.png"><img src="img/persona.png"><img src="img/persona.png">'   
+    },
+];
+
